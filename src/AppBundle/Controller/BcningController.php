3a1ba39108f1b31b580adf8aac014fd807219867
guiba <mas.floris@gmail.com>
2016-11-27 17:29:56 +0000

    added new template and stations list

--- src/AppBundle/Controller/BcningController.php
+++ src/AppBundle/Controller/BcningController.php
@@ -3,7 +3,9 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 
 class BcningController extends Controller
@@ -12,8 +14,27 @@ class BcningController extends Controller
      * @Route("/bcning", name="bcning")
      */
 
-    public function indexAction($name)
+    public function indexAction(Request $request)
     {
-        return $this->render('bcning/index.html.twig');
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://wservice.viabicing.cat/v2/stations');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec($ch);
+
+// If using JSON...
+        $data = json_decode($response, true);
+        dump($data);
+        return $this->render('bcning/index.html.twig', $data);
+   }
+
+    /**
+     * @Route("stations", name="stations")
+     */
+    public function showStationsAction(Request $request)
+    {
+
+        return $this->render('bcning/stations.html.twig');
     }
 }
