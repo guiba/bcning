32f60e1b61a00c03770345db21f1b36b287cd447
guiba <mas.floris@gmail.com>
2016-11-29 15:18:26 +0000

    Added service BicingApi

--- src/AppBundle/Controller/BcningController.php
+++ src/AppBundle/Controller/BcningController.php
@@ -16,39 +16,18 @@ class BcningController extends Controller
 
     public function indexAction(Request $request)
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'http://wservice.viabicing.cat/v2/stations');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        $response = curl_exec($ch);
-
-// If using JSON...
-        $data = json_decode($response, true);
-        $data['tot_stations'] = 0;
-        $data['tot_slots'] = 0;
-        $data['tot_bikes'] = 0;
-//  A bit of stats
-        foreach ($data['stations'] as $station)
-        {
-            $data['tot_stations'] += 1;
-            $data['tot_slots'] += $station['slots'];
-            $data['tot_bikes'] += $station['bikes'];
-
-        }
-
-//        dump($data);
+        $data = $this->get('bicing_api')->getStations();
         return $this->render('bcning/index.html.twig', $data);
    }
 
-    /**
-     * @Route("/stations", name="stations")
-     */
-    public function showStationsAction(Request $request)
-    {
-
-        return $this->render('bcning/stations.html.twig');
-    }
+//    /**
+//     * @Route("/stations", name="stations")
+//     */
+//    public function showStationsAction(Request $request)
+//    {
+//
+//        return $this->render('bcning/stations.html.twig');
+//    }
 
     /**
      * @Route("station/{stationId}", name="station")
---
+++ src/AppBundle/Service/BicingApi.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Service;
+
+class BicingApi
+{
+    public function getStations()
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://wservice.viabicing.cat/v2/stations');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec($ch);
+
+// If using JSON...
+        $data = json_decode($response, true);
+        $data['tot_stations'] = 0;
+        $data['tot_slots'] = 0;
+        $data['tot_bikes'] = 0;
+//  A bit of stats
+        foreach ($data['stations'] as $station)
+        {
+            $data['tot_stations'] += 1;
+            $data['tot_slots'] += $station['slots'];
+            $data['tot_bikes'] += $station['bikes'];
+
+        }
+        return $data;
+    }
+
+    public function getStation($stationId)
+    {
+
+    }
+}
