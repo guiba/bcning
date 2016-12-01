7149836c839d89bb63133501b0157fc974c90d2f
guiba <mas.floris@gmail.com>
2016-12-01 18:41:55 +0000

    Added bicing svg logo instead of red dot

---
+++ tests/AppBundle/Service/BicingApiTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: uto
+ * Date: 30/11/16
+ * Time: 18:26
+ */
+
+namespace tests\AppBoundle\Service;
+
+
+class BicingApiTest extends TestCase
+{
+    public function test()
+    {
+        
+    }
+
+}
