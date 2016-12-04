5895ffd9607b55ce05082d56d6e4c2eeccc6e828
guiba <mas.floris@gmail.com>
2016-12-04 16:58:26 +0000

    Added social login with github

--- src/AppBundle/Controller/BcningController.php
+++ src/AppBundle/Controller/BcningController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
